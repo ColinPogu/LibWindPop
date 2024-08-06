203cf4808ff8dfa7a74d3618128aabebfd799cbf
风语 <10127265+with-wind-and-rain@user.noreply.gitee.com>
2024-08-06 22:05:53 +0800

    字符编码添加缓存字典

--- src/Utils/Extension/EncodingExtension.cs
+++ src/Utils/Extension/EncodingExtension.cs
@@ -1,15 +1,24 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace LibWindPop.Utils.Extension
 {
     internal static class EncodingExtension
     {
+        private static Dictionary<EncodingType, Encoding> _cache = new Dictionary<EncodingType, Encoding>();
+
         public static Encoding GetEncoding(this EncodingType encodingType)
         {
+            if (_cache.TryGetValue(encodingType, out Encoding? ans))
+            {
+                return ans;
+            }
             try
             {
                 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                return Encoding.GetEncoding((int)encodingType);
+                Encoding value = Encoding.GetEncoding((int)encodingType);
+                _cache.Add(encodingType, value);
+                return value;
             }
             catch
             {
