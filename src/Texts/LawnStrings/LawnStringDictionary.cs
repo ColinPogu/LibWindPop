bfef209b5714a7057200d5b9ce2c4541ec6d2694
风语 <10127265+with-wind-and-rain@user.noreply.gitee.com>
2024-08-06 20:56:50 +0800

    添加LawnStrings相关函数

---
+++ src/Texts/LawnStrings/LawnStringDictionary.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LibWindPop.Texts.LawnStrings
+{
+    public class LawnStringDictionary<T1, T2> : IDictionary<T1, T2>
+        where T1 : class
+        where T2 : class
+    {
+        private readonly List<T1> _list1;
+        private readonly List<T2> _list2;
+
+        public LawnStringDictionary()
+        {
+            _list1 = new List<T1>();
+            _list2 = new List<T2>();
+        }
+
+        public T2 this[T1 key]
+        {
+            get => _list2[_list1.IndexOf(key)];
+            set => _list2[_list1.IndexOf(key)] = value;
+        }
+
+        public KeyValuePair<T1, T2> this[int index]
+        {
+            get => new KeyValuePair<T1, T2>(_list1[index], _list2[index]);
+        }
+
+        public ICollection<T1> Keys => _list1;
+
+        public ICollection<T2> Values => _list2;
+
+        public int Count => _list1.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(T1 key, T2 value)
+        {
+            _list1.Add(key);
+            _list2.Add(value);
+        }
+
+        public void Add(KeyValuePair<T1, T2> item)
+        {
+            _list1.Add(item.Key);
+            _list2.Add(item.Value);
+        }
+
+        public void Clear()
+        {
+            _list1.Clear();
+            _list2.Clear();
+        }
+
+        public bool Contains(KeyValuePair<T1, T2> item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_list1[i] == item.Key && _list2[i] == item.Value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool ContainsKey(T1 key)
+        {
+            return _list1.Contains(key);
+        }
+
+        public void CopyTo(KeyValuePair<T1, T2>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<T1, T2>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T1 key)
+        {
+            int index = _list1.IndexOf(key);
+            if (index >= 0)
+            {
+                _list1.RemoveAt(index);
+                _list2.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public bool Remove(KeyValuePair<T1, T2> item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_list1[i] == item.Key && _list2[i] == item.Value)
+                {
+                    _list1.RemoveAt(i);
+                    _list2.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool TryGetValue(T1 key, [MaybeNullWhen(false)] out T2 value)
+        {
+            int index = _list1.IndexOf(key);
+            if (index >= 0)
+            {
+                value = _list2[index];
+                return true;
+            }
+            value = null;
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Texts/LawnStrings/TodStringFile.cs
@@ -0,0 +1,144 @@
+﻿using LibWindPop.Utils.FileSystem;
+using LibWindPop.Utils.Logger;
+using System.IO;
+using System.Text;
+
+namespace LibWindPop.Texts.LawnStrings
+{
+    public static class TodStringFile
+    {
+        public static void Write(LawnStringDictionary<string, string> gStringProperties, string theFileName, IFileSystem fileSystem, ILogger logger)
+        {
+            using (Stream stream = fileSystem.Create(theFileName))
+            {
+                Write(gStringProperties, stream, logger);
+            }
+        }
+
+        public static void Write(LawnStringDictionary<string, string> gStringProperties, Stream stream, ILogger logger)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(stream, encoding: Encoding.Unicode, leaveOpen: true))
+            {
+                int count = gStringProperties.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var pair = gStringProperties[i];
+                    streamWriter.Write('[');
+                    streamWriter.Write(pair.Key);
+                    streamWriter.WriteLine(']');
+                    streamWriter.WriteLine(pair.Value);
+                    streamWriter.WriteLine();
+                }
+            }
+        }
+
+        public static LawnStringDictionary<string, string>? Read(Stream stream, ILogger logger)
+        {
+            LawnStringDictionary<string, string> gStringProperties = new LawnStringDictionary<string, string>();
+            using (StreamReader streamReader = new StreamReader(stream, leaveOpen: true))
+            {
+                string theFileText = streamReader.ReadToEnd();
+                if (!TodStringListReadItems(theFileText, gStringProperties, logger))
+                {
+                    return null;
+                }
+            }
+            return gStringProperties;
+        }
+
+        public static LawnStringDictionary<string, string>? Read(string theFileName, IFileSystem fileSystem, ILogger logger)
+        {
+            using (Stream stream = fileSystem.OpenRead(theFileName))
+            {
+                return Read(stream, logger);
+            }
+        }
+
+        private static bool TodStringListReadName(string theFileText, ref string theName, ref int theStartIndex, ILogger logger)
+        {
+            int aNameStartIndex = theFileText.IndexOf('[', theStartIndex);
+            if (aNameStartIndex == -1)
+            {
+                int spn = StrSpn(theFileText, " \n\r\t", theStartIndex);
+                if (spn != (theFileText.Length - theStartIndex))
+                {
+                    logger.LogError("Failed to find string name");
+                    return false;
+                }
+                theName = string.Empty;
+                return true;
+            }
+            else
+            {
+                int aNameEndIndex = theFileText.IndexOf(']', aNameStartIndex + 1);
+                if (aNameEndIndex == -1)
+                {
+                    logger.LogError("Failed to find ']'");
+                    return false;
+                }
+                int aCount = aNameEndIndex - aNameStartIndex - 1;
+                theName = theFileText.Substring(aNameStartIndex + 1, aCount);
+                if (theName.Length == 0)
+                {
+                    logger.LogError("Name Too Short");
+                    return false;
+                }
+                theStartIndex += aCount + 2;
+                return true;
+            }
+        }
+
+        private static void TodStringRemoveReturnChars(ref string theValue)
+        {
+            theValue = theValue.Replace("\r", "");
+        }
+
+        private static bool TodStringListReadValue(string theFileText, ref string theValue, ref int theStartIndex, ILogger logger)
+        {
+            int aValueEndIndex = theFileText.IndexOf('[', theStartIndex);
+            int aLen = aValueEndIndex != -1 ? (aValueEndIndex - theStartIndex) : (theFileText.Length - theStartIndex);
+            theValue = theFileText.Substring(theStartIndex, aLen).Trim();
+            TodStringRemoveReturnChars(ref theValue);
+            theStartIndex += aLen;
+            return true;
+        }
+
+        private static bool TodStringListReadItems(string theFileText, LawnStringDictionary<string, string> gStringProperties, ILogger logger)
+        {
+            int aPtr = 0;
+            string aName = string.Empty;
+            string aValue = string.Empty;
+            while (true)
+            {
+                if (!TodStringListReadName(theFileText, ref aName, ref aPtr, logger))
+                {
+                    return false;
+                }
+                if (aName.Length == 0)
+                {
+                    return true;
+                }
+                if (!TodStringListReadValue(theFileText, ref aValue, ref aPtr, logger))
+                {
+                    return false;
+                }
+                string aNameUpper = aName.ToUpper();
+                gStringProperties.Add(aNameUpper, aValue);
+            }
+        }
+
+        private static int StrSpn(string InputString, string Mask, int start)
+        {
+            int count = 0;
+            for (int i = start; i < InputString.Length; i++)
+            {
+                if (!Mask.Contains(InputString[i]))
+                {
+                    break;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+}
